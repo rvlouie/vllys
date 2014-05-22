602ce51acbad3368a1a128bfd269bc1bd046b1a2
Ethan Fuchs <ethan@indabamusic.com>
2014-05-22 13:36:04 -0400

    Override scrolling to next/prev slide. Rename everything slide from anchor.

--- js/main.js
+++ js/main.js
@@ -7,12 +7,17 @@ $(document).ready(function(){
     , scrollSpeed
     , parallax
 
-    , anchors = _.collect($('.slide'), function(el){ return el.getAttribute('id'); })
-    , anchorPositions
-    , visibleAnchor
-    , currentAnchor
-    , nextAnchor
-    , previousAnchor
+    , slides = _.collect($('.slide'), function(el){ return el.getAttribute('id'); })
+    , slidePositions
+
+    , currentSlide
+    , nextSlide
+    , previousSlide
+
+    , $currentSlide
+    , $nextSlide
+    , $previousSlide
+
     , currentOffset = 0
     , nextOffset = 0
     , previousOffset = 0
@@ -23,63 +28,89 @@ $(document).ready(function(){
 
     , tempNode;
 
-
-  currentAnchor = (function(){
+  
+  currentSlide = (function(){
     if (window.location.hash) {
-      var matchedAnchorIndex = _.indexOf(anchors, window.location.hash.replace('#', ''));
-      if (matchedAnchorIndex != -1) return anchors[matchedAnchorIndex];
+      var matchedSlideIndex = _.indexOf(slides, window.location.hash.replace('#', ''));
+      if (matchedSlideIndex != -1) return slides[matchedSlideIndex];
     }
-    return anchors[0];
+    return slides[0];
   }());
 
+  $currentSlide = $('#'+currentSlide);
+
+
+  $(window).resize(setSize);
 
   $slides.on('onwheel mousewheel DOMMouseScroll', onScroll);
 
-  setSize()
-  $(window).resize(setSize);
-
-
   Mousetrap.bind(['home'], function(e) {
-    scrollToSlide(anchors[0], 1);
+    scrollToSlide(0);
     return false;
   });
 
   Mousetrap.bind(['end'], function(e) {
-    scrollToSlide(anchors[anchors.length - 1], 1);
+    scrollToSlide(slides.length - 1);
     return false;
   });  
 
   Mousetrap.bind(['up', 'left', 'pageup', 'shift+space'], function(e) {
-    var curIndex = _.indexOf(anchors, currentAnchor);
-    if (curIndex != -1) scrollToSlide(anchors[curIndex - 1]);
+    goToPreviousSlide();
     return false;
   });
   
   Mousetrap.bind(['down', 'right', 'pagedown', 'space'], function(e) {
-    var curIndex = _.indexOf(anchors, currentAnchor);
-    if (curIndex != -1) scrollToSlide(anchors[curIndex + 1]);
+    goToNextSlide();
     return false;
   });
 
 
-  function scrollToSlide(slide, speed) {
+  setSize();
+
+
+
+  function goToNextSlide() {
+    var curIndex = _.indexOf(slides, currentSlide);
+    if (curIndex != -1) scrollToSlide(curIndex + 1);
+  }
+
+  function goToPreviousSlide() {
+    var curIndex = _.indexOf(slides, currentSlide);
+    if (curIndex != -1) scrollToSlide(curIndex - 1);
+  }
+
+  function scrollToSlide(index) {
+    var slide = slides[index];
     if (slide) {
+      currentSlide  = slide;
+      nextSlide     = slides[index + 1];
+      previousSlide = slides[index - 1];
+
+      $currentSlide   = $('#'+currentSlide);
+      $nextSlide      = $('#'+nextSlide);
+      $previousSlide  = $('#'+previousSlide);
+
+      updateHash(slide);
+
+      var $slide = slide;
       $('body, html').stop();
       if (animatingTO) clearTimeout(animatingTO);
       isAnimating = true;
-      $('body, html').animate({
-        scrollTop: $('#'+slide).offset().top
-      }, {
-        duration: speed || 1000,
+
+      var options = {
+        duration: 400,
         complete: function() {
           animatingTO = setTimeout(function() {
             isAnimating = false;
           }, 500);
-        },
-        progress: function() {
-          onScroll();
         }
-      });
+      };
+
+      if (parallax) options.progress = updateParallax
+
+      $('body, html').animate({
+        scrollTop: slidePositions[_.indexOf(slides, slide)]
+      }, options);
     }
   }
 
@@ -87,64 +118,89 @@ $(document).ready(function(){
     tempNode = $('#'+hash);
     if (tempNode) tempNode.attr('id', '');
     window.location.hash = hash;
-    if (tempNode) tempNode.attr('id', currentAnchor);
+    if (tempNode) tempNode.attr('id', hash);
     tempNode = undefined;
   }
 
-  function onScroll(e) {
+  var stopScroll,
+      onScrollTO;
 
-    if (e && isAnimating) {
-      e.preventDefault();
-      return false;
+  function onScroll(e) {
+    if (e) e.preventDefault();
+    if (stopScroll) return;
+
+    var moveX,
+        moveY;
+    
+    if (e.type == 'mousewheel') {
+      moveX = (e.originalEvent.wheelDeltaX * -1);
+      moveY = (e.originalEvent.wheelDeltaY * -1);
     }
+    else if (e.type == 'DOMMouseScroll') {
+      moveX = 0;
+      moveY = e.originalEvent.detail;
+    }
+    else if (e.type == 'onwheel') {
+      moveX = e.originalEvent.deltaX;
+      moveY = e.originalEvent.deltaY;
+    }
+
+    if (moveY > 40) {
+      goToNextSlide();
+    } else if (moveY < -40) {
+      goToPreviousSlide();
+    } else if (moveX > 40) {
+      goToNextSlide();
+    } else if (moveX < -40) {
+      goToPreviousSlide();
+    } else {
+      moveX = false;
+      moveY = false;
+    }
+
+    if (moveX || moveY) {
+      if (onScrollTO) clearTimeout(onScrollTO);
+      stopScroll = true;
+      onScrollTO = setTimeout(function(){
+        stopScroll = false;
+      }, 1000);
+    }
+  }
+
+  function updateParallax() {
 
     pos = $(window).scrollTop();
 
-    visibleAnchor = _.find(anchors, function(anchor, i){
-      if ( anchor && (pos <= anchorPositions[i]) ) {
-        previousAnchor = anchors[i - 1];
-        nextAnchor = anchors[i + 1];
-        return true;
-      } else {
-        return false;
-      }
-    });
+    if (parallax) {
 
-    if (currentAnchor != visibleAnchor) {
-      currentAnchor = visibleAnchor;
-      updateHash(currentAnchor);
-    }
-    
-    if ($(window).width() >= 800) {
-
-      if ($('#'+currentAnchor).length) {
-        currentOffset = ( windowHeight / 2 - ( $('#'+currentAnchor).offset().top - pos ) ) / windowHeight - 0.5;
+      if ($currentSlide && $currentSlide.length) {
+        currentOffset = ( windowHeight / 2 - ( $currentSlide.offset().top - pos ) ) / windowHeight - 0.5;
         if (currentOffset >= -1 && currentOffset <= 1) {
-          $('#'+currentAnchor+' .container').css({ marginTop: scrollSpeed * currentOffset });
+          $currentSlide.find('.container').css({ marginTop: scrollSpeed * currentOffset });
         }
       }
       
-      if ($('#'+nextAnchor).length) {
-        nextOffset = ( windowHeight / 2 - ( $('#'+nextAnchor).offset().top - pos ) ) / windowHeight - 0.5;
+      if ($nextSlide && $nextSlide.length) {
+        nextOffset = ( windowHeight / 2 - ( $nextSlide.offset().top - pos ) ) / windowHeight - 0.5;
         if (nextOffset >= -1 && nextOffset <= 1) {
-          $('#'+nextAnchor+' .container').css({ marginTop: scrollSpeed * nextOffset });
+          $nextSlide.find('.container').css({ marginTop: scrollSpeed * nextOffset });
         }
       }
 
-      if ($('#'+previousAnchor).length) {
-        previousOffset = ( windowHeight / 2 - ( $('#'+previousAnchor).offset().top - pos ) ) / windowHeight - 0.5;
+      if ($previousSlide && $previousSlide.length) {
+        previousOffset = ( windowHeight / 2 - ( $previousSlide.offset().top - pos ) ) / windowHeight - 0.5;
         if (previousOffset >= -1 && previousOffset <= 1) {
-          $('#'+previousAnchor+' .container').css({ marginTop: scrollSpeed * previousOffset });
+          $previousSlide.find('.container').css({ marginTop: scrollSpeed * previousOffset });
         }
       }
 
     }
   }
   
-  function getAnchorPositions(as) {
-    return _.unique(_.collect(as, function(anchor) {
-      if (anchor) {
-        return $('#'+anchor).offset().top;
+  function getSlidePositions(as) {
+    return _.unique(_.collect(as, function(slide) {
+      if (slide) {
+        return $('#'+slide).offset().top;
       } else {
         return 0;
       }
@@ -166,12 +222,10 @@ $(document).ready(function(){
       });
     });
 
-    scrollSpeed = windowHeight < 800 ? 100 : 200;
+    scrollSpeed = windowHeight < 800 ? 120 : 280;
     parallax = windowWidth > 800;
 
-    anchorPositions = getAnchorPositions(anchors);
-    
-    scrollToSlide(currentAnchor);
+    slidePositions = getSlidePositions(slides);
   }
 
 
